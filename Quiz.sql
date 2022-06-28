f2170b76e20fdc5ff50e14b99f21c8abd74db7d9
Akash GR <akashgr64@gmail.com>
2022-06-28 23:34:10 +0530

    database added

--- Quiz.sql
+++ Quiz.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `college_google_form`
+-- Database: `Quiz`
 --
 
 -- --------------------------------------------------------
